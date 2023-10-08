f062d5a90d61cff629e62223a675bb313fbcebaf
sailingamigos <sailingramigos@gmail.com>
2023-10-08 20:57:49 +0200

    Add plugin code

---
+++ plugin/main.py
@@ -0,0 +1,44 @@
+"""Main module"""
+
+import json
+import quart
+import quart_cors
+from maestro import get_answer, load_data
+from quart import request, jsonify
+
+app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
+
+@app.post("/ask")
+async def ask_question():
+    """Ask a question"""
+    data = await request.get_json()
+    response = get_answer(data)
+    return jsonify(response)
+
+@app.get("/logo.jpg")
+async def plugin_logo():
+    """Return plugin logo"""
+    filename = './.well-known/logo.jpg'
+    return await quart.send_file(filename, mimetype='image/png')
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    """Return plugin manifest"""
+    with open("./.well-known/ai-plugin.json", encoding="utf-8") as file:
+        text = file.read()
+        return quart.Response(text, mimetype="text/json")
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    """Return plugin API specification"""
+    with open("./.well-known/openapi.yaml", encoding="utf-8") as file:
+        text = file.read()
+        return quart.Response(text, mimetype="text/yaml")
+
+def main():
+    """Main function"""
+    load_data ('./assets_bot/cache/assets.jsonld')
+    app.run(debug=True, host="localhost", port=3000)
+
+if __name__ == "__main__":
+    main()
